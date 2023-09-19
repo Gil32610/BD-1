380b9848fb8316067a8208cb320b21402456ae26
GIL32610 <gil.00000032610@unicap.br>
2023-09-19 15:21:11 -0300

    menu class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import UserFunc.*;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Menu.menuList();
     }
 }
---
+++ src/UserFunc/Menu.java
@@ -0,0 +1,17 @@
+package UserFunc;
+
+public class Menu {
+    public static void menuList() {
+        System.out.println("-".repeat(20));
+        System.out.println("Informe a opção desejada:");
+        System.out.println("1 - Criar novo registro");
+        System.out.println("2 - Listar registros");
+        System.out.println("3 - Alterar registros");
+        System.out.println("4 - Remover Registros");
+        System.out.println("5 - Consultar Registros");
+        System.out.println("0 - Encerrar");
+        System.out.println("-".repeat(20));
+    }
+
+    
+}
