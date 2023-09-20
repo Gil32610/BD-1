dcdf7529d4df11e48bd03cc50b6c18086484f934
GIL32610 <gil.00000032610@unicap.br>
2023-09-20 09:52:15 -0300

    testing file

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ import UserFunc.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Menu.menuList();
+        System.out.println("Current Working Directory = " + new File("").getAbsolutePath());
         String filename = "files/data_set_library.csv";
         File file = new File(filename);
         System.out.printf("%s",file.exists()?"It's here": "Was not created");
